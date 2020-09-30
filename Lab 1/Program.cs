beb0dc659ff3c1de9c391baf6b924bd91c82d896
BohdanAnisimov <71391499+BohdanAnisimov@users.noreply.github.com>
2020-09-30 10:21:47 +0300

    Add files via upload

---
+++ Lab 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Лаб. работа №1, Вариант 2
+            float U;
+            float I;
+            float R;
+            string str;
+            Console.WriteLine("Введите значение напряжения:");
+            str = Console.ReadLine();
+            U = Convert.ToInt32(str);
+            Console.WriteLine("Введите значение сопротивления:");
+            str = Console.ReadLine();
+            R = Convert.ToInt32(str);
+            I = U / R;
+            Console.WriteLine($"Сила тока: {I}");
+            Console.ReadKey();
+        }
+    }
+}
