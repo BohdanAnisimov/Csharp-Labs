688b0deef52535769bdc7c5bd217a4143a1176b6
BohdanAnisimov <71391499+BohdanAnisimov@users.noreply.github.com>
2020-11-22 21:04:35 +0300

    Add files via upload

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    /*Створити ліст трінгових змінних, дозволити можливість заповнення з калвіатури. 
+     * Вивести індекси змінних рівних перевірочній(теж ввести з клавіатури). Скопіювати ліст в масив.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Console.WriteLine("Введите пять слов (eng):");
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            Console.WriteLine("Введите слово для проверки (eng):");
+            string word = Console.ReadLine();
+            for (int i = 0; i < 5; i++)
+            {
+                if (word == list[i])
+                {
+                    Console.WriteLine($"Слово {list[i]} имеет индекс {i}.");
+                }
+            }
+            string[] array = new string[5];
+            for(int i = 0; i < 5; i++)
+            {
+                array[i] = list[i];
+            }
+            Console.WriteLine("Скопированный массив слов: ");
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
